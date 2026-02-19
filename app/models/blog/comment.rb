d891e2049d656213a2b58c3ff7c431d0a750a974
devsherm <deven@shermanbuildings.com>
2026-02-19 04:40:53 -0600

    add length constraints to text fields

--- app/models/blog/comment.rb
+++ app/models/blog/comment.rb
@@ -1,4 +1,5 @@
 class Blog::Comment < ApplicationRecord
   belongs_to :post
-  validates :author, :body, presence: true
+  validates :author, presence: true, length: { maximum: 100 }
+  validates :body, presence: true, length: { maximum: 10_000 }
 end
--- app/models/blog/post.rb
+++ app/models/blog/post.rb
@@ -1,5 +1,7 @@
 class Blog::Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
-  validates :title, :body, :author, presence: true
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :body, presence: true, length: { maximum: 50_000 }
+  validates :author, presence: true, length: { maximum: 100 }
 end
