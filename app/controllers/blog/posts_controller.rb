9c3fcab2b75e732b2f1fb16703b7b8926ca1214c
devsherm <deven@shermanbuildings.com>
2026-02-19 06:05:29 -0600

    harden posts

--- app/controllers/blog/posts_controller.rb
+++ app/controllers/blog/posts_controller.rb
@@ -1,10 +1,14 @@
 class Blog::PostsController < ApplicationController
-  rate_limit to: 5, within: 1.minute, only: :create
+  # before_action :require_authentication, except: %i[ index show ]
   before_action :set_blog_post, only: %i[ show edit update destroy ]
+  # before_action :authorize_post_owner!, only: %i[ edit update destroy ]
+
+  rate_limit to: 5, within: 1.minute, only: %i[ create update ]
+  rate_limit to: 3, within: 1.minute, only: :destroy
 
   # GET /blog/posts or /blog/posts.json
   def index
-    @blog_posts = Blog::Post.order(created_at: :desc)
+    @blog_posts = Blog::Post.order(created_at: :desc).limit(25).offset(pagination_offset)
   end
 
   # GET /blog/posts/1 or /blog/posts/1.json
@@ -31,7 +35,7 @@ class Blog::PostsController < ApplicationController
         format.json { render :show, status: :created, location: @blog_post }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @blog_post.errors, status: :unprocessable_entity }
+        format.json { render json: { errors: @blog_post.errors.full_messages }, status: :unprocessable_entity }
       end
     end
   end
@@ -44,18 +48,23 @@ class Blog::PostsController < ApplicationController
         format.json { render :show, status: :ok, location: @blog_post }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @blog_post.errors, status: :unprocessable_entity }
+        format.json { render json: { errors: @blog_post.errors.full_messages }, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /blog/posts/1 or /blog/posts/1.json
   def destroy
-    @blog_post.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to blog_posts_path, notice: "Post was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
+    if @blog_post.destroy
+      respond_to do |format|
+        format.html { redirect_to blog_posts_path, notice: "Post was successfully destroyed.", status: :see_other }
+        format.json { head :no_content }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to @blog_post, alert: "Post could not be destroyed.", status: :see_other }
+        format.json { render json: { error: "Post could not be destroyed." }, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -65,8 +74,22 @@ class Blog::PostsController < ApplicationController
       @blog_post = Blog::Post.find(params.expect(:id))
     end
 
+    # Verify the current user owns the post before allowing mutations.
+    def authorize_post_owner!
+      unless @blog_post.author == current_user.name
+        respond_to do |format|
+          format.html { redirect_to @blog_post, alert: "You are not authorized to perform this action.", status: :see_other }
+          format.json { render json: { error: "Forbidden" }, status: :forbidden }
+        end
+      end
+    end
+
     # Only allow a list of trusted parameters through.
     def blog_post_params
       params.expect(blog_post: [ :title, :body, :topic, :author ])
+    end
+
+    def pagination_offset
+      [params.fetch(:page, 0).to_i, 0].max * 25
     end
 end
