7c0cc796303aefd6733155f7badd5fa7a1a6d756
devsherm <deven@shermanbuildings.com>
2026-02-19 06:00:24 -0600

    add length check to topic

--- app/models/blog/post.rb
+++ app/models/blog/post.rb
@@ -3,5 +3,6 @@ class Blog::Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 50_000 }
+  validates :topic, length: { maximum: 100 }
   validates :author, presence: true, length: { maximum: 100 }
 end
